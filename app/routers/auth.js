2211e3ec4bbe0b29b6ae8bc34366d9a4e4f5b434
Gwenael Cotton <cotton.g@sfeir.com>
2022-10-24 10:04:25 +0200

    update auth login

--- app/routers/auth.js
+++ app/routers/auth.js
@@ -8,5 +8,6 @@ const router = new express.Router();
 router.post('/register', validate(userRegisterSchema), authController.register);
 router.post('/login', authController.login);
 router.post('/refreshToken', authController.refreshToken);
+// router.post('/refreshToken', checkUserIsLogged, authController.refreshToken);
 
 module.exports = router;
