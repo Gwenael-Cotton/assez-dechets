fd17181499bf3c9a2d979531f8d418d3872da5aa
blandinejar <blandine.jarysta@gmail.com>
2022-10-24 11:26:57 +0200

    remove commented out routes

--- app/routers/event.js
+++ app/routers/event.js
@@ -6,11 +6,6 @@ const { checkUserIsLogged } = require('../middleware/access');
 
 const router = new express.Router();
 
-// router.get('/events', checkUserIsLogged, eventController.getAllEvents);
-// router.get('/events/:id', checkUserIsLogged, eventController.getOneEvent);
-// router.post('/events', checkUserIsLogged, eventController.createEvent);
-// router.put('/events/:id', checkUserIsLogged, eventController.updateEvent);
-// router.delete('/events/:id', checkUserIsLogged, eventController.deleteEvent);
 router.get('/events', checkUserIsLogged, eventController.getAllEvents);
 router.get('/events/:id', checkUserIsLogged, eventController.getOneEvent);
 router.post('/events', checkUserIsLogged, validate(eventSchema), eventController.createEvent);
