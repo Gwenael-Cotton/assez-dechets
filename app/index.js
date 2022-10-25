91ab84202cfecdb52f7b4aaf258a09cbbdc5db5a
Gwenael Cotton <cotton.g@sfeir.com>
2022-10-25 15:29:29 +0200

    add credentials cors

--- app/index.js
+++ app/index.js
@@ -5,13 +5,14 @@ const eventRouter = require('./routers/event');
 const authRouter = require('./routers/auth');
 const userRouter = require('./routers/user');
 const errorRouter = require('./routers/error');
-const { setDecodedToken } = require('./middleware/access');
+// const { setDecodedToken } = require('./middleware/access');
 
 const app = express();
 
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN_URL,
+    credentials: true,
   }),
 );
 app.use(express.json());
@@ -19,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api', authRouter);
-app.use(setDecodedToken);
+// app.use(setDecodedToken);
 app.use('/api', eventRouter);
 app.use('/api', userRouter);
 app.use('*', errorRouter);
