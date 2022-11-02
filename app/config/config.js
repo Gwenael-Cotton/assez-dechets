d2580f6271ffa63c2d199dec4acaf012e0b54326
Gwenael Cotton <cotton.g@sfeir.com>
2022-11-02 01:18:48 +0100

    try to fix test CI/CD workflow file

--- app/config/config.js
+++ app/config/config.js
@@ -9,12 +9,20 @@ module.exports = {
     dialect: 'postgres',
   },
   test: {
-    username: 'test',
-    password: 'testPassword',
-    database: 'test',
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
     host: process.env.PGHOST,
     dialect: 'postgres',
   },
+  // test: {
+  //   username: 'assez-dechets-test',
+  //   password: 'testPassword',
+  //   database: 'assez-dechets-project-test',
+  //   host: 'localhost',
+  //   ports: 5433,
+  //   dialect: 'postgres',
+  // },
   production: {
     username: process.env.PGUSER,
     password: process.env.PGPASSWORD,
